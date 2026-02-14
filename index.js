e87bde98c6e2e10e92a3c2521e4c2dd7d7b41557
FranciscoMadrid <ffms-a@outlook.com>
2026-02-14 00:04:55 -0600

    Added brand's model, route and controller and updated vehicle and log queries.

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import cors from 'cors';
 import VehicleRoute from './routes/vehicle.route.js';
 import LogRoute from './routes/log.route.js';
 import ModelRoute from "./routes/model.route.js"
+import BrandRoute from "./routes/brand.route.js"
+import 'dotenv/config';
 
 const app = express();
+const PORT = process.env.PORT || 3000; 
 
 app.use(cors({
-  origin: ['http://localhost:4321', 'https://your-production-site.com'],
+  origin: [`http://localhost:4321`, 'https://flowdrive.ffmsdev.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -18,8 +21,8 @@ app.use(express.json());
 app.use('/api/vehicle', VehicleRoute);
 app.use('/api/log', LogRoute);
 app.use('/api/model', ModelRoute)
+app.use('/api/brand', BrandRoute)
 
-const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
